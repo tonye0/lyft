79e211e9605386a4ff10224d43a90f2c2d22fe78
Fortune Minaseidiema <fminaseidiema26@gmail.com>
2024-01-23 02:37:50 +0100

    first commit

---
+++ components/engine/capulet_engine.py
@@ -0,0 +1,13 @@
+# from lyft.car import Engine
+# import willoughby_engine
+
+from car import testing
+
+
+# class CapuletEngine(Engine):
+#     def __init__(self, last_service_mileage, current_mileage):
+#         self.last_service_mileage = last_service_mileage
+#         self.current_mileage = current_mileage  
+
+#     def needs_service(self):
+#         return self.current_mileage - self.last_service_mileage > 30000
